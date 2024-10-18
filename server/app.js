cd90971280ab75da063738cf811202fdbf18faaa
RohanGorla <rohangorla2010@gmail.com>
2024-10-19 01:20:49 +0530

    add upload to drive function to convert file buffer data into a buffer stream to upload to google drive

--- server/app.js
+++ server/app.js
@@ -22,19 +22,55 @@ const auth = new google.auth.JWT(process.env.CE, null, process.env.PK, [
 
 const drive = google.drive({ version: "v3", auth: auth });
 
+async function uploadToDrive(file, userName) {
+  const { buffer } = file;
+  const fileName = `${userName}-${Date.now()}`;
+  function bufferToStream(buffer) {
+    const readable = new Readable();
+    readable.push(buffer);
+    readable.push(null);
+    return readable;
+  }
+  const metadata = {
+    name: fileName,
+    parents: [process.env.ID],
+  };
+
+  const media = {
+    mimeType: file.mimetype,
+    body: bufferToStream(buffer),
+  };
+
+  try {
+    const response = await drive.files.create({
+      resource: metadata,
+      media,
+      fields: "*",
+    });
+    return response.data;
+  } catch (error) {
+    console.error(`Error uploading ${userName}:`, error.message);
+    throw error;
+  }
+}
+
 app.get("/", (req, res) => {
   res.json("listening");
 });
 
-app.post("/upload", upload.single("images"), async (req, res) => {
+app.post("/upload", upload.single("files"), async (req, res) => {
   try {
-    console.log(req.files);
-    const file = req.files[0];
-    console.log(file);
+    const file = req.file;
+    const { userName } = req.body;
+    console.log("File -> ", file);
+    console.log("User Name -> ", userName);
+
+    const fileData = await uploadToDrive(file, userName);
 
     res.send({
       access: true,
-      successMsg: "File uploaded successfully"
+      successMsg: "File uploaded successfully",
+      data: fileData,
     });
   } catch (error) {
     res.send({
