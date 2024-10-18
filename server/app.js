efe5e4e31b3945e75edf00a979ef912dd47f977c
RohanGorla <rohangorla2010@gmail.com>
2024-10-18 23:53:29 +0530

    set up multer storage and create google service account auth for accessing Google drive

--- server/app.js
+++ server/app.js
@@ -5,12 +5,22 @@ import multer from "multer";
 import { google } from "googleapis";
 import dotenv from "dotenv";
 import { Readable } from "stream";
+dotenv.config()
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 const PORT = process.env.PORT || 3000;
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+const auth = new google.auth.JWT(process.env.CE, null, process.env.PK, [
+  "https://www.googleapis.com/auth/drive.file",
+]);
+
+const drive = google.drive({ version: "v3", auth: auth });
 
 app.get("/", (req, res) => {
   res.json("listening");
