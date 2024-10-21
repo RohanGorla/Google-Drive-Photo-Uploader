25af30f363047e455d3fb3d384c7ec7993aad813
RohanGorla <rohangorla2010@gmail.com>
2024-10-21 21:56:26 +0530

    Add upload form with username input, select & upload buttons and empty input error messages

--- client/src/Components/Homepage.jsx
+++ client/src/Components/Homepage.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { FaHeart } from "react-icons/fa";
 import { FaImages } from "react-icons/fa";
+import { MdUpload } from "react-icons/md";
+import { MdPhoto } from "react-icons/md";
 import imageCompression from "browser-image-compression";
 import axios from "axios";
 import data from "./Base64images";
@@ -11,55 +13,73 @@ function Homepage() {
   const [userName, setUserName] = useState("");
   const [showLoadingPage, setShowLoadingPage] = useState(true);
   const [greetingNote, setGreetingNote] = useState("");
+  const [userNameError, setUserNameError] = useState(false);
+  const [filesQuantityError, setFilesQuantityError] = useState(false);
+  const uploadRef = useRef(null);
   const message =
     "Thank you for being part of our special day! Your presence means the world to us as we begin this exciting journey together. We hope you enjoyed the celebration as much as we did. Please share your captured moments with us by uploading your photos below!";
 
   async function handleSubmit() {
-    const folderCreationResponse = await axios.post(
-      `${import.meta.env.VITE_BASE_URL}/createfolder`,
-      {
-        folderName: userName,
-      }
-    );
-    console.log(folderCreationResponse);
-    if (folderCreationResponse.data.access) {
-      let count = 1;
-      const folderId = folderCreationResponse.data.data.id;
-      console.log(folderId);
-      for (const file of files) {
-        let compressedFile;
-        if (file.size / (1024 * 1024) > 4) {
-          const options = {
-            maxSizeMB: 4,
-            useWebWorker: true,
-          };
-          compressedFile = await imageCompression(file, options);
-        } else {
-          compressedFile = file;
+    if (userName.length && files.length) {
+      const folderCreationResponse = await axios.post(
+        `${import.meta.env.VITE_BASE_URL}/createfolder`,
+        {
+          folderName: userName,
         }
-        console.log(
-          `Folder ${count} out of ${files.length} -> `,
-          compressedFile.size / (1024 * 1024),
-          "MB"
-        );
-        let formData = new FormData();
-        formData.append("files", compressedFile);
-        formData.append("userName", userName);
-        formData.append("folderId", folderId);
-        const fileUploadResponse = await axios.post(
-          `${import.meta.env.VITE_BASE_URL}/upload`,
-          formData,
-          {
-            headers: {
-              "Content-Type": "multipart/form-data",
-            },
+      );
+      console.log(folderCreationResponse);
+      if (folderCreationResponse.data.access) {
+        let count = 1;
+        const folderId = folderCreationResponse.data.data.id;
+        console.log(folderId);
+        for (const file of files) {
+          let compressedFile;
+          if (file.size / (1024 * 1024) > 4) {
+            const options = {
+              maxSizeMB: 4,
+              useWebWorker: true,
+            };
+            compressedFile = await imageCompression(file, options);
+          } else {
+            compressedFile = file;
           }
-        );
-        console.log(fileUploadResponse);
-        count += 1;
+          console.log(
+            `Folder ${count} out of ${files.length} -> `,
+            compressedFile.size / (1024 * 1024),
+            "MB"
+          );
+          let formData = new FormData();
+          formData.append("files", compressedFile);
+          formData.append("userName", userName);
+          formData.append("folderId", folderId);
+          const fileUploadResponse = await axios.post(
+            `${import.meta.env.VITE_BASE_URL}/upload`,
+            formData,
+            {
+              headers: {
+                "Content-Type": "multipart/form-data",
+              },
+            }
+          );
+          console.log(fileUploadResponse);
+          count += 1;
+        }
+      } else {
+        console.log(`${folderCreationResponse.data.error}`);
       }
+      setUserNameError(false);
+      setFilesQuantityError(false);
     } else {
-      console.log(`${folderCreationResponse.data.error}`);
+      if (userName.length == 0) {
+        setUserNameError(true);
+      } else {
+        setUserNameError(false);
+      }
+      if (files.length == 0) {
+        setFilesQuantityError(true);
+      } else {
+        setFilesQuantityError(false);
+      }
     }
   }
 
@@ -68,14 +88,14 @@ function Homepage() {
     if (greetingNote.length) {
       setTimeout(() => {
         if (greetingNote.length === message.length) {
-          setTimeout(() => {
-            setGreetingNote(message[0]);
-          }, 3000);
+          // setTimeout(() => {
+          // setGreetingNote(message);
+          // }, 3000);
         } else {
           let updatedMessage = greetingNote + message[greetingNote.length];
           setGreetingNote(updatedMessage);
         }
-      }, 70);
+      }, 20);
     }
   }, [greetingNote]);
 
@@ -84,6 +104,10 @@ function Homepage() {
       setShowLoadingPage(false);
       setGreetingNote(message[0]);
     }, [8500]);
+    const scrollDelay = message.length * 20 + 1000 + 8500;
+    setTimeout(() => {
+      uploadRef.current?.scrollIntoView({ behavior: "smooth" });
+    }, scrollDelay);
   }, []);
 
   return (
@@ -128,29 +152,78 @@ function Homepage() {
             <p className="HomePage_Greetings--Note">{greetingNote}</p>
           </div>
         </article>
-        <section className="Input_Form">
-          <div className="Files_Input">
+        <section className="HomePage_Upload_Container" ref={uploadRef}>
+          <div className="HomePage_Upload_Container--Tint"></div>
+          <p className="HomePage_Upload_Container--Note">
+            Share your cherished moments from the event, so we can treasure them
+            for years to come!
+          </p>
+          <div className="HomePage_Upload_Folder_Name">
+            <label className="HomePage_Upload_Folder_Name--Label">
+              Your Name:
+            </label>
             <input
-              type="file"
-              accept="image/*"
-              name="files"
-              multiple
-              onChange={(e) => {
-                setFiles(e.target.files);
-              }}
-            ></input>
-          </div>
-          <div className="Name_Input">
-            <input
+              className="HomePage_Upload_Folder_Name--Input"
               type="text"
+              placeholder="Enter your name..."
               onChange={(e) => {
                 setUserName(e.target.value);
               }}
             ></input>
           </div>
-          <button onClick={handleSubmit} className="Submit_Button">
-            Submit
-          </button>
+          <p
+            className={
+              userNameError
+                ? "HomePage_Upload_Folder_Name_Empty"
+                : "HomePage_Upload_Folder_Name_Empty--Inactive"
+            }
+          >
+            * Your name is required for folder creation!
+          </p>
+          <div className="HomePage_Upload_Select_And_Upload_Container">
+            <p className="HomePage_Upload_Select_And_Upload--Label">
+              Select & Upload your images:
+            </p>
+            <div className="HomePage_Upload_Select_And_Upload">
+              <div className="HomePage_Upload_Image_Selector">
+                <label
+                  className="HomePage_Upload_Image_Selector--Label"
+                  htmlFor="imagesUpload"
+                >
+                  <MdPhoto size={22} /> <span>Select</span>
+                </label>
+                <input
+                  type="file"
+                  accept="image/*"
+                  id="imagesUpload"
+                  name="files"
+                  multiple
+                  onChange={(e) => {
+                    setFiles(e.target.files);
+                  }}
+                  style={{ display: "none" }}
+                ></input>
+              </div>
+              <div
+                className="HomePage_Upload_Submit_Button"
+                onClick={handleSubmit}
+                // onClick={() => {
+                //   console.log("click");
+                // }}
+              >
+                <MdUpload size={22} /> <span>Upload</span>
+              </div>
+            </div>
+          </div>
+          <p
+            className={
+              filesQuantityError
+                ? "HomePage_Upload_Image_Select_Empty"
+                : "HomePage_Upload_Image_Select_Empty--Inactive"
+            }
+          >
+            * Please select atleast 1 image!
+          </p>
         </section>
       </div>
     </div>
