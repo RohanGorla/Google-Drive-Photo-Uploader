2b35d7926a3f1e9424592a0d20f079d985e6d324
RohanGorla <rohangorla2010@gmail.com>
2024-10-26 22:43:56 +0530

    Add update folders data in gallery page

--- client/src/Components/Gallery.jsx
+++ client/src/Components/Gallery.jsx
@@ -16,14 +16,29 @@ function Gallery() {
   const navigate = useNavigate();
   const folderRef = useRef();
 
+  /* Get All Folders Data */
+
+  async function getAllFolders() {
+    const getFilesResponse = await axios.get(
+      `${import.meta.env.VITE_BASE_URL}/getallfolders`
+    );
+    console.log(getFilesResponse.data.data);
+    setAllFoldersData(getFilesResponse.data.data);
+  }
+
+  /* Scroll To Just Opened Folder */
+
   useEffect(() => {
     if (folderRef.current) {
       folderRef.current.scrollIntoView({ behavior: "smooth" });
     }
   }, [currentFolder]);
 
+  /* Set The Current Folder To Just Visited Folder ID */
+
   useEffect(() => {
     sessionStorage.setItem("prevLoc", location.key);
+    getAllFolders();
     const currentFolderId = sessionStorage.getItem("currentFolderId");
     if (currentFolderId) {
       setCurrentFolder(currentFolderId);
@@ -47,35 +62,48 @@ function Gallery() {
             src={`data:image/jpeg;base64,${data.bgImage}`}
           ></img>
         </div>
-        <section className="Gallery_Folders">
-          {allFoldersData?.map((folder, index) => {
-            return (
-              <div
-                ref={folder.id == currentFolder ? folderRef : null}
-                key={index}
-                className="Folder_Container"
-                onClick={() => {
-                  sessionStorage.setItem("currentFolderName", folder.name);
-                  sessionStorage.setItem("currentFolderId", folder.id);
-                  navigate(`folder/${folder.id}`);
-                }}
-              >
-                <div className="Folder--Tint"></div>
-                <p className="Folder_Name">{folder.name}</p>
-                <div className="Folder_View_Button_Container">
-                  <div className="Folder_View_Button">
-                    <div className="Folder_View_Button--Tint"></div>
-                    <FaRegFolderOpen
-                      className="Folder_View_Button--Icon"
-                      size={22}
-                    />
-                    <p className="Folder_View_Button--Text">View</p>
+        {allFoldersData ? (
+          <section className="Gallery_Folders">
+            {allFoldersData?.map((folder, index) => {
+              return (
+                <div
+                  ref={folder.id == currentFolder ? folderRef : null}
+                  key={index}
+                  className="Folder_Container"
+                  onClick={() => {
+                    sessionStorage.setItem("currentFolderName", folder.name);
+                    sessionStorage.setItem("currentFolderId", folder.id);
+                    navigate(`folder/${folder.id}`);
+                  }}
+                >
+                  <div className="Folder--Tint"></div>
+                  <p className="Folder_Name">{folder.name}</p>
+                  <div className="Folder_View_Button_Container">
+                    <div className="Folder_View_Button">
+                      <div className="Folder_View_Button--Tint"></div>
+                      <FaRegFolderOpen
+                        className="Folder_View_Button--Icon"
+                        size={22}
+                      />
+                      <p className="Folder_View_Button--Text">View</p>
+                    </div>
                   </div>
                 </div>
-              </div>
-            );
-          })}
-        </section>
+              );
+            })}
+          </section>
+        ) : (
+          <section className="Gallery_Folders--Empty">
+            <div className="Gallery_Folders--Empty_Message_Container">
+              <div className="Gallery_Folders--Empty_Message_Container--Tint"></div>
+              <p className="GalleryFolders--Empty_Message">
+                There are no folders present yet. Be the first to start the
+                collection by uploading your photos and creating a showcase
+                everyone will enjoy!
+              </p>
+            </div>
+          </section>
+        )}
       </div>
     </>
   );
